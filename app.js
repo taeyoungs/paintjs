88b1a2a317ce18b999d3ec24d74773a50d83e8ef
taeyoung <xoxodudwkd@gmail.com>
2019-09-03 10:39:16 +0900

    add strokeStyle change func

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+
+// 실제 픽셀 사이즈 부여
+// CSS는 우리 눈에 보이는 캔버스의 사이즈를 설정한 것이다.
+canvas.width = 500;
+canvas.height = 500;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -29,13 +36,20 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+if(colors) {
+    Array.from(colors).forEach(color => 
+        color.addEventListener("click", handleColorClick));
+}
+
