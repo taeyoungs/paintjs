7c149f56a685d4feeeafd7c44d1da521220b002f
taeyoung <xoxodudwkd@gmail.com>
2019-09-03 11:16:47 +0900

    add paint to fill, fill to paint func

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const DEFAULT_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 500;
 
 // 실제 픽셀 사이즈 부여
 // CSS는 우리 눈에 보이는 캔버스의 사이즈를 설정한 것이다.
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.fillStyle = DEFAULT_COLOR;
 ctx.lineWidth = 2.5;
 
 // 마우스를 클릭했을 때 painting이 시작되도록
 // 그리고 마우스 드래그를 해제했을 때 멈추도록 + 캔버스를 벗어났을 때
 
 let painting = false;
+// fill, paint의 상태를 결정지어주는 변수를 하나 생성
+let filling = false;
 
 function startPainting() {
     painting = true;
@@ -39,9 +48,32 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Painting";
+    }
+}
+
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
 }
 
 if(canvas) {
+    canvas.addEventListener("click", handleCanvasClick);
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
@@ -53,3 +85,10 @@ if(colors) {
         color.addEventListener("click", handleColorClick));
 }
 
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick);
+}
